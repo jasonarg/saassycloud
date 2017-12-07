1d78188d03111a20147b427a97ca30a5707e08a2
jason <jason.ch.ross@gmail.com>
2017-12-06 20:45:06 -0500

    Added robots noindex meta tag to all pages

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -3,6 +3,7 @@
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
 <meta name="csrf-token" content="{{ csrf_token() }}">
 <title>SaaSsy Cloud | A demo site</title>
+<meta name="robots" content="noindex">
 <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css?family=VT323:400,500,900" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css?family=Fjalla+One:400,600" rel="stylesheet">
